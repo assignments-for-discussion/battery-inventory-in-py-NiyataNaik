3f018d07d4d32fe556ac87d2b7c588573a6cd898
Niyata N. Naik <131361604+NiyataNaik@users.noreply.github.com>
2023-12-03 17:37:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 
 def count_batteries_by_health(present_capacities):
+  healthy_count=0
+  exchange_count=0
+  failed_count=0
+
+  for present_capacity in present_capacities:
+    rated_capacity=120
+    soh=(present_capacity/rated_capacity)
+
+    if soh>75 and soh<=100:
+      healthy_count+=1
+    elif soh>=50 and soh<=75:
+      exchange_count+=1
+    elif soh<50:
+      failed_count+=1
   return {
-    "healthy": 0,
-    "exchange": 0,
-    "failed": 0
+    "healthy": healthy_count,
+    "exchange": exchange_count,
+    "failed": failed_count
   }
 
 
